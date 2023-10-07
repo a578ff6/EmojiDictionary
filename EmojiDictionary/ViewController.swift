1ee0067b586953ba73b0eb61e756c56f8db19274
wayne tsao <a578ff6@gmail.com>
2023-10-07 21:14:41 +0800

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by 曹家瑋 on 2023/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
